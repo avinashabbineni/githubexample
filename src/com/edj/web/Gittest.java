2864708c07743622ca79049a06983bc3abcccf4d
t <t@>
2017-05-03 18:13:20 -0500

    first checkin

--- src/com/edj/web/Gittest.java
+++ src/com/edj/web/Gittest.java
@@ -5,6 +5,7 @@ public class Gittest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("I am in git app");
+        System.out.println("test1");
 	}
 
 }
