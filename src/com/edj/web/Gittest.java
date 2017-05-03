36c629834ecf2b203125509b2f778569eb1d4348
t <t>
2017-05-03 18:33:53 -0500

    test2

--- src/com/edj/web/Gittest.java
+++ src/com/edj/web/Gittest.java
@@ -6,6 +6,7 @@ public class Gittest {
 		// TODO Auto-generated method stub
         System.out.println("I am in git app");
         System.out.println("test1");
+        System.out.println("test2");
 	}
 
 }
