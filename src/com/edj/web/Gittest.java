eb0b1364ba9b8bdaefd5a329e0b61f214bc76c12
test <test@>
2017-05-03 17:57:55 -0500

    github example

---
+++ src/com/edj/web/Gittest.java
@@ -0,0 +1,10 @@
+package com.edj.web;
+
+public class Gittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("I am in git app");
+	}
+
+}
